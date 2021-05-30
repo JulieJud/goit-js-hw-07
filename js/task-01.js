0c16075c8d6bbc4250735bb4509ab151333b7705
Julie Judachina <iuliia.saunova@email.cz>
2021-05-31 01:14:34 +0200

    task1

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const totalNavCategories = document.querySelectorAll(".item");
+console.log(`В списке ${totalNavCategories .length} категории.`);
+
+const categoriesArray = [...totalNavCategories]
+  .map(
+    items => `Категория: ${items.children[0].textContent}
+Количество элементов: ${items.children[1].children.length}`
+  )
+  
+console.log(categoriesArray);
