c70b8553f84596b72a7df293d04e2caec804d7dc
Julie Judachina <iuliia.saunova@email.cz>
2021-06-07 00:39:46 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -5,13 +5,20 @@ validationInput.addEventListener('change', controlOfValidation);
 
 function controlOfValidation(event) {
     console.log(event.currentTarget.value);
-    if (event.currentTarget.value.length == inputLength) {
+  if (event.currentTarget.value.length == inputLength) {
+      
     console.log (typeof (event.currentTarget.value.length))
      console.log(typeof(inputLength))     
     validationInput.classList.add('valid');
     validationInput.classList.remove('invalid');
        
     }
+   else if (event.currentTarget.value.length === 0) {
+   
+   validationInput.classList.remove("valid");
+    validationInput.classList.remove("invalid");
+  }
+
     else
     {
         
