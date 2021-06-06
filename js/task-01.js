53866b305da311dbc1ee8f79fdb3773fd9b5f529
Julie Judachina <iuliia.saunova@email.cz>
2021-06-06 22:59:39 +0200

    1task-change on forEach

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,8 @@
 const totalNavCategories = document.querySelectorAll(".item");
 console.log(`В списке ${totalNavCategories .length} категории.`);
 
-const categoriesArray = [...totalNavCategories]
-  .map(
-    items => `Категория: ${items.children[0].textContent}
-Количество элементов: ${items.children[1].children.length}`
+const categoriesArray = totalNavCategories
+  .forEach(
+  items => console.log( `Категория: ${items.children[0].textContent}
+Количество элементов: ${items.children[1].children.length}`)
   )
-  .join("\n");
-console.log(categoriesArray);
