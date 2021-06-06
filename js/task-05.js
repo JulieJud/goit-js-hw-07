8f0cce15dfb1db8841b127bd7b53d8371282204e
Julie Judachina <iuliia.saunova@email.cz>
2021-06-07 00:18:45 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -4,8 +4,5 @@ const outputNameEl = document.querySelector('#name-output');
 iputNameEl.addEventListener('input',onInputChange)
 function onInputChange(event) {
     console.log(event.currentTarget.value);
-    event.currentTarget.value !== '' ?
-        (outputNameEl.textContent = event.currentTarget.value) :
-        (outputNameEl.textContent='незнакомец') 
-    
+    outputNameEl.textContent = event.currentTarget.value.trim()  || 'незнакомец';
 }
